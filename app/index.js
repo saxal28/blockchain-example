dbac3ee8471032271603ba1ed883d0337195bc74
Alan Sax <alan@lelander.com>
2018-06-06 11:41:27 -0500

    added express server. added /mine and /block routes

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Blockchain = require("../blockchain");
+
+const HTTP_PORT =  process.env.PORT || 4001;
+
+const app = express();
+
+const bc = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send(
+    `
+        <h1>This is the Blockchain Example App</h1>
+        <a href="/blocks">Click here to see all blocks</a>
+    `
+    )
+})
+
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+})
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New Block added: ${block.toString()}`)
+
+    res.redirect("/blocks");
+})
+
+app.listen(HTTP_PORT, () => {
+    console.log(`listening on port ${HTTP_PORT}`)
+})
